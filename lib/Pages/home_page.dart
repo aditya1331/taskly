16ba5ff4213b20eaf6af6733ce304a057419c3c0
ADITYA 2022 <aditya,deshpande1331@gmail.com>
2022-12-11 23:16:29 +0530

    First commit with the start of statefull widget

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  HomePage();
+  @override
+  State<StatefulWidget> createState() {
+    return _home_page();
+
+
+  }
+
+}
+class _home_page extends State<HomePage>{
+  _home_page();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+
+}
