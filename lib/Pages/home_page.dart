6410415bb08dbf9707606b14b7cde3ba0f199385
ADITYA 2022 <aditya,deshpande1331@gmail.com>
2022-12-20 00:39:53 +0530

    Creating a text field to take the task ? attached to data type means it is not mandatory for the variable to have a value Obtaining the value of textfield

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -14,10 +14,12 @@ class HomePage extends StatefulWidget{
 class _home_page extends State<HomePage>{
   //_home_page();
   late double deviceHeight,deviceWidth;
+  String? _newTaskContent;
   @override
   Widget build(BuildContext context) {
     deviceHeight = MediaQuery.of(context).size.height;
     deviceWidth = MediaQuery.of(context).size.width;
+    print("Input value $_newTaskContent");
 
 
     return Scaffold(
@@ -52,9 +54,30 @@ Widget _taskList()
 
 Widget _addTask()
 {
-  return FloatingActionButton(onPressed: (){},
+  return FloatingActionButton(
+    onPressed: _displayTaskPopup,
     child: const Icon(Icons.add,),
 
+  );
+
+}
+
+void _displayTaskPopup()
+{
+  showDialog(context: context,builder: (BuildContext _context)
+  {
+    return AlertDialog(
+        title: const Text("Add new Task"),
+        content: TextField(
+        onSubmitted: (_value){},
+        onChanged: (_value){
+          setState(() {
+            _newTaskContent = _value;
+          });// This function calls the build function indicating the state has been changed
+        },
+      ),
+    );
+  },
   );
 
 }
